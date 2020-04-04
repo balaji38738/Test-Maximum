04a59430857752e370cd4d6cc9fd74c0435f8eb6
balaji38738 <bijjapwar123@gmail.com>
2020-04-04 16:53:48 +0530

    ADDED: Code to find maximum of three floats

--- src/main/java/com/bridgelabz/testmaximum/MaxOfThree.java
+++ src/main/java/com/bridgelabz/testmaximum/MaxOfThree.java
@@ -1,22 +1,40 @@
 package com.bridgelabz.testmaximum;
 
 public class MaxOfThree {
-    private Integer firstNum, secondNum, thirdNum;
-    private int maximum;
+    private Integer firstInteger, secondInteger, thirdInteger;
+    private int intMaximum;
+    private Float firstFloat, secondFloat, thirdFloat;
+    private float floatMaximum;
 
-    public MaxOfThree(Integer firstNum, Integer secondNum,
-                      Integer thirdNum) {
-        this.firstNum = firstNum;
-        this.secondNum = secondNum;
-        this.thirdNum = thirdNum;
+    public MaxOfThree(Integer firstInteger, Integer secondInteger,
+                      Integer thirdInteger) {
+        this.firstInteger= firstInteger;
+        this.secondInteger = secondInteger;
+        this.thirdInteger = thirdInteger;
     }
 
-    public int findMaxOfThree() {
-        int comparison1 = firstNum.compareTo(secondNum);
-        int comparison2 = secondNum.compareTo(thirdNum);
-        int comparison3 = firstNum.compareTo(thirdNum);
-        maximum = comparison1 > 0 ? (comparison3 > 0 ? firstNum : thirdNum ) :
-                (comparison2 > 0 ? secondNum : thirdNum);
-        return maximum;
+    public MaxOfThree(Float firstFloat, Float secondFloat,
+                      Float thirdFloat) {
+        this.firstFloat = firstFloat;
+        this.secondFloat = secondFloat;
+        this.thirdFloat = thirdFloat;
+    }
+
+    public int findMaxOfThreeIntegers() {
+        int comparison1 = firstInteger.compareTo(secondInteger);
+        int comparison2 = secondInteger.compareTo(thirdInteger);
+        int comparison3 = firstInteger.compareTo(thirdInteger);
+        intMaximum = comparison1 > 0 ? (comparison3 > 0 ? firstInteger : thirdInteger ) :
+                (comparison2 > 0 ? secondInteger : thirdInteger);
+        return intMaximum;
+    }
+
+    public float findMaxOfThreeFloats() {
+        int comparison1 = firstFloat.compareTo(secondFloat);
+        int comparison2 = secondFloat.compareTo(thirdFloat);
+        int comparison3 = firstFloat.compareTo(thirdFloat);
+        floatMaximum = comparison1 > 0 ? (comparison3 > 0 ? firstFloat : thirdFloat ) :
+                (comparison2 > 0 ? secondFloat : thirdFloat);
+        return floatMaximum;
     }
 }
--- src/test/java/com/bridgelabz/testmaximum/MaxOfThreeTest.java
+++ src/test/java/com/bridgelabz/testmaximum/MaxOfThreeTest.java
@@ -8,22 +8,45 @@ public class MaxOfThreeTest {
     @Test
     public void givenThreeIntegers_whenMaxAtFirst_shouldReturnTheSame() {
         MaxOfThree maxOfThree = new MaxOfThree(5,2,4);
-        int maximum = maxOfThree.findMaxOfThree();
+        int maximum = maxOfThree.findMaxOfThreeIntegers();
         Assert.assertEquals(5, maximum);
     }
 
     @Test
     public void givenThreeIntegers_whenMaxAtSecond_shouldReturnTheSame() {
         MaxOfThree maxOfThree = new MaxOfThree(3, 4, 1);
-        int maximum = maxOfThree.findMaxOfThree();
+        int maximum = maxOfThree.findMaxOfThreeIntegers();
         Assert.assertEquals(4, maximum);
     }
 
     @Test
     public void givenThreeIntegers_whenMaxAtThird_shouldReturnTheSame() {
         MaxOfThree maxOfThree = new MaxOfThree(11, 22, 33);
-        int maximum = maxOfThree.findMaxOfThree();
+        int maximum = maxOfThree.findMaxOfThreeIntegers();
         Assert.assertEquals(33, maximum);
     }
 
+    @Test
+    public void givenThreeFloats_whenMaxAtFirst_shouldReturnTheSame() {
+        MaxOfThree maxOfThree = new MaxOfThree(3.14f, 1.414f, 2.73f);
+        float maximum = maxOfThree.findMaxOfThreeFloats();
+        boolean isEqual = maximum == 3.14f;
+        Assert.assertTrue(isEqual);
+    }
+
+    @Test
+    public void givenThreeFloat_whenMaxAtSecond_shouldReturnTheSame() {
+        MaxOfThree maxOfThree = new MaxOfThree(1.6f, 9.8f, 6.14f);
+        float maximum = maxOfThree.findMaxOfThreeFloats();
+        boolean isEqual = maximum == 9.8f;
+        Assert.assertTrue(isEqual);
+    }
+
+    @Test
+    public void givenThreeFloat_whenMaxAtThird_shouldReturnTheSame() {
+        MaxOfThree maxOfThree = new MaxOfThree(1.6f, 9.8f, 9.8f);
+        float maximum = maxOfThree.findMaxOfThreeFloats();
+        boolean isEqual = maximum == 9.8f;
+        Assert.assertTrue(isEqual);
+    }
 }
